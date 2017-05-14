6eb75f95c6819dae479a52e614714bf7e2877067
Volodya <prokopiv.v@gmail.com>
2017-05-14 00:14:55 +0000

    add new file

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function() {
+    
+    $('a.navmenu').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+    
+});
